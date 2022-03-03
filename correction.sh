ea9a81a81ad38a4fd5dee701c269d5c25b1c4db5
Endlessio <117010423@link.cuhk.edu.cn>
2022-03-03 16:52:35 -0500

    test

--- correction.sh
+++ correction.sh
@@ -3,4 +3,4 @@
 # Edit as necessary for different interpreters and/or file locations
 # Note: "$@" is functionally-equivalent to "$1 $2 $3 ..."
 # Come to hours or post on Piazza if you have questions about scripting!
-python3 src/correction.py $1 $2 $3 $4 $5
+python3 src/correction.py $1 $2 $3 $4
